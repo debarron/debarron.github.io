3281c0c6db6750277a90073ea5959512e07e6488
debarron <de.lopezbarron@gmail.com>
2017-05-24 04:18:10 -0500

    Updating the external reviews list

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+$(document).ready(function() {
+    var myItems;
+
+    $.getJSON('assets/external_reviews.json', function(data) {
+        myItems = data.items;
+
+        myItems.forEach(function(element){
+          console.log(myItems);
+        });
+    });
+});

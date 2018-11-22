bdf03c8ac8f237dd66c129830830aeb55f81f6c8
debarron <de.lopezbarron@gmail.com>
2018-11-21 18:40:11 -0600

    Adding awards

--- javascripts/test1.js
+++ javascripts/test1.js
@@ -1,21 +1,6 @@
 console.log('This would be the main JS file.');
 var externalReviews = 'https://debarron.github.io/assets/external_reviews.json';
-var conferences = 'https://debarron.github.io/assets/conferences.json';
-var awards = 'https://debarron.github.io/assets/awards.json';
-
-// function to write external reviews
-// function to write publications
-// function to write awards
-function appendAwards(jsonAwards){
-  var awardsHTML = ""
-  jsonAwards.forEach(function(year){
-    awardsHTML += "<ul><strong>" + year + "</strong>"
-
-    year.forEach(function(award){awardsHTML += "<li>" + award + "<li>"});
-
-    awardsHTML += "</ul>"
-  });
-}
+var conferences = 'https://debarron.github.io/assets/conferences.json'
 
 
 $(document).ready(function() {

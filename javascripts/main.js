eda7142a55c1a624feefb42f7433ee5036ad6cbf
debarron <de.lopezbarron@gmail.com>
2018-12-17 13:42:08 -0800

    Adding the conferences

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,19 +17,44 @@ function appendAwards(jsonAwards){
   });
 }
 
+function appendReviews(data){
+  data.forEach(function(reviews){
+    $("#external-review").append("<ul><strong>" +reviews.year + "</strong>");
+    
+    reviews.conferences.forEach(function(conf){
+      $("#external-review").append("<ul>" + conf + "</ul>");
+    });
+    $("#external-review").append("</ul>");
+  });
+}
+
+function comparePublications(a, b){
+  if(a.year < b.year) return -1;
+  if(a.year > b.year) return 1;
+  return 0;
+}
+
+function appendConferences(data){
+  var htmlFormat = "";
+  var publications = data.publications.sort(comparePublications);
+
+  return publications.lenght
+}
 
 $(document).ready(function() {
 
     // Get the list of external reviews
-    $.getJSON(externalReviews, function(data) {
-        data.forEach(function(reviews){
-          $("#external-review").append("<ul><strong>" +reviews.year + "</strong>");
-          reviews.conferences.forEach(function(conf){
-            $("#external-review").append("<ul>" + conf + "</ul>");
-          });
-          $("#external-review").append("</ul>");
-        });
-    });
+    $.getJSON(externalReviews, appendReviews);
+    $.getJSON(conferences, appendConferences);
+  //  $.getJSON(externalReviews, function(data) {
+  //      data.forEach(function(reviews){
+  //        $("#external-review").append("<ul><strong>" +reviews.year + "</strong>");
+  //        reviews.conferences.forEach(function(conf){
+  //          $("#external-review").append("<ul>" + conf + "</ul>");
+  //        });
+  //        $("#external-review").append("</ul>");
+  //      });
+  //  });
 
     
 });
